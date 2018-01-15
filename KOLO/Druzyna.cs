97fcc1c7f65ea936bb63452b2422ceb0aa82f631
lukaszrafalo <35449689+lukaszrafalo@users.noreply.github.com>
2018-01-15 10:26:11 +0100

    Add files via upload

---
+++ KOLO/Druzyna.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KOLO
+{
+    class Druzyna:IDruzyna
+    {
+       
+        private Pilkarz[,] druzyna = new Pilkarz[2, 4];
+        druzyna[0,0] = ("Adam","Nowak",12);
+        druzyna[0,1] = ("Andrzej","Tamal",2);
+        private string nazwa;
+
+       public bool CzyOK()
+        {
+           if(druzyna != null)
+           {
+               return true;
+           }
+           else
+           {
+               return false;
+           }
+       }
+        public void UstawNazwe(string nazwa)
+        {
+            this.nazwa=nazwa;
+        }
+    
+        public string ToString()
+        {
+            return druzyna;
+        }
+}
+}
---
+++ KOLO/IDruzyna.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KOLO
+{
+    interface IDruzyna
+    {
+        void UstawNapastnika();
+
+        void UstawObronce();
+
+        void UstawNazwe();
+
+        bool CzyOK();
+    }
+}
